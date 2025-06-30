dd42c9c164a7119ad7b9079403d8bf1325c1a7fe
anramosp <anramosp.ext@acciona.com>
2025-06-30 10:14:36 +0200

    update

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from fastapi.responses import JSONResponse
 from sentence_transformers import SentenceTransformer
 import torch
 import time
-import os
+import base64
+from io import BytesIO
+from typing import List
+import numpy as np
+import cv2
+from PIL import Image
 
 app = FastAPI()
 
@@ -26,6 +31,48 @@ async def get_embeddings(request: Request):
         print(f"⏳ [Get Embeddings] Tiempo de ejecución: {time.perf_counter() - start_time:.4f} segundos")
 
         return {"tags": tags, "embeddings": embeddings}
+
+    except Exception as e:
+        print(e)
+        return JSONResponse(status_code=500, content={"error": "Internal Server Error"})
+
+@app.post("/color-histograms")
+async def get_color_histograms(request: Request):
+    try:
+        data = await request.json()
+        images = data.get("images", [])
+
+        if not images or not isinstance(images, list):
+            return JSONResponse(status_code=400, content={"error": "Field 'images' must be a list."})
+
+        hue_bins = 16
+        sat_bins = 4
+
+        start_time = time.perf_counter()
+        results = []
+
+        for item in images:
+            idx = item.get("id")
+            base64_image = item.get("base64")
+
+            if not idx or not base64_image:
+                continue
+
+            image_data = base64.b64decode(base64_image)
+            img = Image.open(BytesIO(image_data)).convert("RGB")
+
+            img_cv = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+            hsv_image = cv2.cvtColor(img_cv, cv2.COLOR_BGR2HSV)
+
+            hist = cv2.calcHist([hsv_image], [0, 1], None, [hue_bins, sat_bins], [0, 180, 0, 256])
+            hist = cv2.normalize(hist, hist).flatten()
+            histogram_vector = hist.tolist()
+
+            results.append({"id": idx, "embedding": histogram_vector})
+
+        print(f"⏳ [Get Color Histograms] Tiempo de ejecución: {time.perf_counter() - start_time:.4f} segundos")
+
+        return {"embeddings": results}
 
     except Exception as e:
         print(e)
